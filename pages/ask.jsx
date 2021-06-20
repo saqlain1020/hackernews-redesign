7df09b422d5520bbaa752744f787a4318355c9e9
Saqlain1020 <saqlainprinters@gmail.com>
2021-06-20 13:50:47 +0500

    build error fixes

--- pages/ask.jsx
+++ pages/ask.jsx
@@ -1,51 +1,56 @@
-import { useState } from "react";
+import React from "react";
 import Head from "next/head";
 import getStories from "../lib/getStories";
 import Page from "../components/Page";
 
-export async function getStaticProps() {
-  const query = await getStories("askstories", lastVisible);
-  lastVisible = query.docs[query.docs.length - 1] || null;
-  let posts = [];
-  query.forEach((doc) => {
-    let obj = doc.data();
-    obj.id = doc.id;
-    obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
-    posts.push(obj);
-  });
-  console.log(posts)
-  return { props: { posts } };
-}
+// export async function getStaticProps() {
+//   const query = await getStories("frontstories", lastVisible);
+//   lastVisible = query.docs[query.docs.length - 1] || null;
+//   let posts = [];
+//   query.forEach((doc) => {
+//     let obj = doc.data();
+//     obj.id = doc.id;
+//     obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
+//     posts.push(obj);
+//   });
+//   console.log(posts);
+//   return { props: { posts } };
+// }
 
-export default function Ask({ posts }) {
-  const [pageCount, setpageCount] = useState(1);
-  const pages = [];
+export default function Home() {
+  const [stories, setStories] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [last, setLast] = React.useState(null);
+  const getData = async () => {
+    setLoading(true);
+    const query = await getStories("ask", last);
+    let posts = [];
+    setLast(query.docs[query.docs.length - 1] || last);
+    query.forEach((doc) => {
+      let obj = doc.data();
+      obj.id = doc.id;
+      obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
+      posts.push(obj);
+    });
+    setStories([...stories, ...posts]);
+    setLoading(false);
+  };
 
-  for (let i = 0; i < pageCount; i++) {
-    pages.push(
-      <Page
-        page={i + 1}
-        category="askstories"
-        initialData={i + 1 !== 1 ? null : posts}
-        key={i}
-      />
-    );
-  }
+  React.useEffect(getData, []);
 
   return (
     <>
       <Head>
-        <title>Hackernews Redesign - Ask stories</title>
+        <title>Hackernews Redesign - A HN client built on top of Next.js</title>
+        <meta property="og:image" content="/hn-redesign.png" />
       </Head>
       <div className="lg:col-span-2 mt-8">
         <span className="main-title flex items-center text-soft-black">
-          <h1 className="fancy-undeline">Ask stories</h1>
+          <h1 className="fancy-undeline">Top stories</h1>
         </span>
-        {pages}
-        <button
-          className="more-btn"
-          onClick={() => setpageCount(pageCount + 1)}
-        >
+        <Page category="frontstories" stories={stories || []} />
+        {loading && <h1>Loading...</h1>}
+        <button className="more-btn" onClick={getData}>
           Load more
         </button>
       </div>
--- pages/new.jsx
+++ pages/new.jsx
@@ -1,51 +1,56 @@
-import { useState } from "react";
+import React from "react";
 import Head from "next/head";
 import getStories from "../lib/getStories";
 import Page from "../components/Page";
 
-export async function getStaticProps() {
-  const query = await getStories("newstories", lastVisible);
-  lastVisible = query.docs[query.docs.length - 1] || null;
-  let posts = [];
-  query.forEach((doc) => {
-    let obj = doc.data();
-    obj.id = doc.id;
-    obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
-    posts.push(obj);
-  });
-  console.log(posts)
-  return { props: { posts } };
-}
+// export async function getStaticProps() {
+//   const query = await getStories("frontstories", lastVisible);
+//   lastVisible = query.docs[query.docs.length - 1] || null;
+//   let posts = [];
+//   query.forEach((doc) => {
+//     let obj = doc.data();
+//     obj.id = doc.id;
+//     obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
+//     posts.push(obj);
+//   });
+//   console.log(posts);
+//   return { props: { posts } };
+// }
 
-export default function New({ posts }) {
-  const [pageCount, setpageCount] = useState(1);
-  const pages = [];
+export default function Home() {
+  const [stories, setStories] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [last, setLast] = React.useState(null);
+  const getData = async () => {
+    setLoading(true);
+    const query = await getStories("", last);
+    let posts = [];
+    setLast(query.docs[query.docs.length - 1] || last);
+    query.forEach((doc) => {
+      let obj = doc.data();
+      obj.id = doc.id;
+      obj.createdAt = new Date(obj.createdAt.toDate()).toDateString();
+      posts.push(obj);
+    });
+    setStories([...stories, ...posts]);
+    setLoading(false);
+  };
 
-  for (let i = 0; i < pageCount; i++) {
-    pages.push(
-      <Page
-        page={i + 1}
-        category="newstories"
-        initialData={i + 1 !== 1 ? null : posts}
-        key={i}
-      />
-    );
-  }
+  React.useEffect(getData, []);
 
   return (
     <>
       <Head>
-        <title>Hackernews Redesign - New stories</title>
+        <title>Hackernews Redesign - A HN client built on top of Next.js</title>
+        <meta property="og:image" content="/hn-redesign.png" />
       </Head>
       <div className="lg:col-span-2 mt-8">
         <span className="main-title flex items-center text-soft-black">
-          <h1 className="fancy-undeline">New stories</h1>
+          <h1 className="fancy-undeline">Top stories</h1>
         </span>
-        {pages}
-        <button
-          className="more-btn"
-          onClick={() => setpageCount(pageCount + 1)}
-        >
+        <Page category="frontstories" stories={stories || []} />
+        {loading && <h1>Loading...</h1>}
+        <button className="more-btn" onClick={getData}>
           Load more
         </button>
       </div>
